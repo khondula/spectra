14fa67f46c8648734f5b4745f13ef19935ab6979
Kelly Hondula (US) <hondula@battelleecology.org>
2021-03-17 17:20:42 -0600

    plotting spectra and maps

--- 04-extract-polygon.R
+++ 04-extract-polygon.R
@@ -8,38 +8,33 @@ library(hdf5r)
 library(sf)
 library(tidyverse)
 library(glue)
+library(terra)
+library(stars)
+library(scico)
 source('R/myfxns.R')
 
 # set up
-spectra_dir <- 'H:/DATA/spectra/'
+spectra_dir <- 'H:/DATA/spectra_buff5m'
 shp_dir <- 'H:/DATA/spatial'
 
 # inputs
-my_aq_site <- 'PRLA'
+my_aq_site <- 'SUGG'
 my_aop_yr <- '2019'
 my_loc_type <- 'buoy.c0'
+nbands <- 100
 
-get_aop_dates <- function(aq_siteids){
-  aop_file <- 'results/sites_join_aop_dates.csv'
-  aop_dates <- readr::read_csv(aop_file, col_types = 'ccccccccddD') %>%
-    dplyr::filter(siteID %in% aq_siteids) %>%
-    dplyr::select(siteID, aop_site_id, flightdate) %>%
-    dplyr::arrange(flightdate) %>% distinct()
-  return(aop_dates)
-}
+# to get AOP name for AQ site
+my_aop_site <- 'results/sites_join_aop_dates.csv' %>%
+  readr::read_csv(col_types = 'ccccccccddD') %>%
+  dplyr::filter(siteID %in% my_aq_site) %>%
+  dplyr::pull(aop_site_id) %>% unique()
 
-# AOP site name
-my_aop_site <- get_aop_dates(my_aq_site) %>% 
-  pull(aop_site_id) %>% unique()
-
-# read in points
+# read in point for AQ site
 my_aq_sf <- glue('{shp_dir}/swchem_sites/swchem_sites.shp') %>%
   st_read() %>% dplyr::filter(siteID == my_aq_site) %>%
-  mutate(location_type = substr(nmdLctn, 10, nchar(nmdLctn)))
-my_loc_types <- my_aq_sf$location_type
-my_domain <- my_aq_sf$domanID[1]
-my_aq_pt_sf <- my_aq_sf %>% 
+  mutate(location_type = substr(nmdLctn, 10, nchar(nmdLctn))) %>% 
   dplyr::filter(location_type == my_loc_type)
+my_domain <- my_aq_sf$domanID[1]
 
 # folder with AOP data
 my_site_dir <- glue('D:/{my_aop_yr}/FullSite/{my_domain}') %>%
@@ -53,16 +48,17 @@ my_h5 <- hdf5r::H5File$new(my_h5_file, mode = "r")
 epsg_path <- glue('{my_aop_site}/Reflectance/Metadata/Coordinate_System/EPSG Code')
 my_epsg <- my_h5[[epsg_path]]$read() %>% as.integer()
 
-# only for one point
-my_aq_prj <- my_aq_pt_sf %>% st_transform(crs = my_epsg)
-# buffer point
-my_aq_prj_sp <- as(my_aq_prj, "Spatial")
+# project and buffer point
+my_aq_prj <- my_aq_sf %>% st_transform(crs = my_epsg)
 my_aq_prj_buff5 <- st_buffer(my_aq_prj, 5)
+# convert to sp objects
+my_aq_prj_sp <- as(my_aq_prj, "Spatial")
 my_buff5_sp <- as(my_aq_prj_buff5, "Spatial")
-
+# get corners of extent
 bbox_pts_sf <- my_aq_prj_buff5 %>% 
   st_bbox() %>% st_as_sfc() %>% 
   st_as_sf() %>% st_cast(to = "POINT")
+my_xys <- sf::st_coordinates(bbox_pts_sf) %>% as.data.frame()
 
 # find the images that span the entire buffered point
 my_site_files_df <- my_site_files %>%
@@ -74,10 +70,6 @@ my_site_files_df <- my_site_files %>%
          northing = as.numeric(str_sub(filename, 26, 32)),
          northing2 = northing + 1000)
 
-# find which file one point is in
-my_xys <- sf::st_coordinates(bbox_pts_sf) %>% as.data.frame()
-# my_id <- 1
-# need my_xys, my_site_files_df
 find_which_file <- function(my_id){
   my_x <- my_xys[['X']][my_id]
   my_y <- my_xys[['Y']][my_id]
@@ -93,70 +85,80 @@ my_h5_files <- 1:nrow(my_xys) %>%
   purrr::map(~find_which_file(.x)) %>% 
   unlist() %>% unique()
 
+# read in and merge h5 files using hs_read
 if(length(my_h5_files) == 1){
-  my_h5 <- hs_read(my_h5_files, bands = 1:100)
+  my_h5 <- hs_read(my_h5_files, bands = 1:nbands)
+  names(my_h5) <- hs_wavelength(my_h5_files, bands = 1:nbands)
 }
 if(length(my_h5_files) > 1){
-  my_h5 <- hs_read(my_h5_files[1], bands = 1:100)
-  my_hs_list <- purrr::map(my_h5_files, ~hs_read(.x, bands = 1:100))
+  my_h5 <- hs_read(my_h5_files[1], bands = 1:nbands)
+  names(my_h5) <- hs_wavelength(my_h5_files, bands = 1:nbands)
+  my_hs_list <- purrr::map(my_h5_files, ~hs_read(.x, bands = 1:nbands))
   for(i in 2:length(my_h5_files)){
     my_h5 <- merge(my_h5, my_hs_list[[i]])
   }
 }
 
-my_buff5_sp <- as(my_aq_prj_buff5, "Spatial")
-my_aq_prj_sp <- as(my_aq_prj, "Spatial")
-plot(my_h5[[100]], col = cividis(100), axes = FALSE, box = FALSE,
-     main = glue('{my_aq_site} {my_aop_yr} \n {names(my_h5)[3]}'))
-plot(my_buff5_sp, add = TRUE, border = 'green', col = NA)
-plot(my_aq_prj_sp, add = TRUE, col = 'red', pch = 3)
-
-##
-my_h5 <- hdf5r::H5File$new(my_h5_file, mode = "r")
-hdf5r::h5close(my_h5)
-
-my_hs <- hs_read(my_h5_file, bands = c(1, 50, 100, 400))
-plot(my_hs[[3]], col = cividis(100), axes = FALSE, box = FALSE,
-     main = glue('{my_aq_site} {my_aop_yr} \n {names(my_hs)[3]}'))
-
-
 # extract spectra from pixels within the 5m buffered point
-my_h5
-library(terra)
 my_pt_spatvec <- my_aq_prj_sp %>% as('SpatVector')
 my_pts_spatvec <- my_buff5_sp %>% as('SpatVector')
 
-#raster::extract(my_h5, my_aq_prj_sp)
-
-# my_hs <- hs_read(my_h5_file, bands = 1:200) # or 1:426
-# this is slow. faster with terra?
-# didnt work with all bands...
-names(my_h5) <- hs_wavelength(my_h5_file, bands = 1:100)
-
 my_hs_terra <- my_h5 %>% terra::rast()
+
 # find which cell the exact coords of sampling
+my_cellid <- cells(my_hs_terra, my_pt_spatvec)[2]
 
 my_spectra <- terra::extract(my_hs_terra, my_pts_spatvec, cells = TRUE)
 my_spectra_df <- my_spectra %>% 
   as_tibble() %>%
+  mutate(x = terra::xFromCell(my_hs_terra, cell),
+         y = terra::yFromCell(my_hs_terra, cell)) %>%
   group_by(cell) %>%
   pivot_longer(cols = starts_with('band'), 
                names_to = 'band', 
                values_to = 'reflectance') %>% 
   tidyr::separate(band, into = c('index', 'wavelength'), sep = "_") %>%
-  dplyr::mutate(wavelength = parse_number(wavelength))
+  dplyr::mutate(wavelength = parse_number(wavelength)) %>%
+  dplyr::mutate(my_cellid = cell == my_cellid)
+
+my_spectra_pt <- dplyr::filter(my_spectra_df, my_cellid)
 
 my_spectra_df %>%
   ggplot(aes(wavelength, reflectance)) +
   geom_line(aes(group = cell)) +
+  geom_line(data = my_spectra_pt, col = 'green', lwd = 1) +
   theme_bw() +
+  # coord_cartesian(ylim = c(0, 0.1)) +
   theme(legend.position = 'none') +
   ggtitle(glue('{my_aq_site}-{my_aop_yr}-5mbuff-buoyc0'))
 
 ggsave(glue('{my_aq_site}-{my_aop_yr}-5mbuff-buoyc0.png'))
-ggsave('prpo2019-5mbuff-buoyc0.png')
 
-# make layers of cell IDs and or XYs
-# extract all bands
-# OR convert all pixel centroids to use hs_extract 
+my_spectra_df %>% 
+  write_csv(glue('{spectra_dir}/{my_aq_site}_{my_aop_yr}_{str_replace_all(my_loc_type, "[:punct:]", "")}-buff5m.csv'))
 
+
+bands_to_plot <- seq(1, 100, length.out = 12)
+my_h5 <- hs_read(my_h5_files, bands = bands_to_plot)
+names(my_h5) <- hs_wavelength(my_h5_files, bands = bands_to_plot)
+
+my_aq_prj_buff <- st_buffer(my_aq_prj, 5)
+my_buff_sp <- as(my_aq_prj_buff, "Spatial")
+my_aq_prj_buff30 <- st_buffer(my_aq_prj, 25)
+my_buff30_sp <- as(my_aq_prj_buff30, "Spatial")
+
+my_hs_crop <- raster::crop(my_h5, my_buff30_sp)
+hs_crop_stars <- my_hs_crop %>% st_as_stars()
+
+gg <- ggplot() +
+  geom_stars(data = hs_crop_stars) +
+  coord_equal() +
+  facet_wrap(~band) +
+  geom_sf(data = my_aq_prj, pch = 3, col = 'green') +
+  geom_sf(data = my_aq_prj_buff, fill = NA, col = 'white') +
+  theme_void() + 
+  theme(legend.title = element_blank()) +
+  scale_fill_scico(palette = 'davos') +
+  ggtitle(glue('{my_aq_site} {my_aop_yr} {my_loc_type}'))
+
+ggsave(glue('figs/buff5m-maps/{my_aq_site}_{my_aop_yr}_{str_replace_all(my_loc_type, "[:punct:]", "")}.pdf'), plot = gg, width = 10, height = 8)
