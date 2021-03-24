ec5907a70ae06b9c8ab62f32d2bf26c43109743b
Kelly Hondula (US) <hondula@battelleecology.org>
2021-03-24 17:21:33 -0600

    convert datetime info to actual datetime

--- 00-radiance-time.R
+++ 00-radiance-time.R
@@ -9,8 +9,8 @@ library(rhdf5)
 # library(terra)
 # library(stars)
 # library(scico)
-source('R/myfxns.R')
-
+# source('R/myfxns.R')
+source('R/hs_mapinfo.R')
 
 # set up
 shp_dir <- 'H:/DATA/spatial'
@@ -19,7 +19,6 @@ shp_dir <- 'H:/DATA/spatial'
 my_aq_site <- 'BLWA'
 my_aop_yr <- '2017'
 my_loc_type <- 'buoy.c0'
-# nbands <- 100
 
 # to get AOP name for AQ site
 my_aop_site <- 'results/sites_join_aop_dates.csv' %>%
@@ -45,6 +44,7 @@ my_h5_file <- my_site_files[1]
 my_h5 <- hdf5r::H5File$new(my_h5_file, mode = "r")
 epsg_path <- glue('{my_aop_site}/Radiance/Metadata/Coordinate_System/EPSG Code')
 my_epsg <- my_h5[[epsg_path]]$read() %>% as.integer()
+my_h5$close_all()
 
 # project and buffer point
 my_aq_prj <- my_aq_sf %>% st_transform(crs = my_epsg)
@@ -57,10 +57,9 @@ bbox_pts_sf <- my_aq_prj_buff5 %>%
   st_bbox() %>% st_as_sfc() %>% 
   st_as_sf() %>% st_cast(to = "POINT")
 my_xys <- sf::st_coordinates(bbox_pts_sf) %>% as.data.frame()
+my_xys <- sf::st_coordinates(my_aq_prj) %>% as.data.frame()
 
 # find the images that span the entire buffered point
-source('R/hs_mapinfo.R')
-
 my_site_files_df <- my_site_files %>%
   as.data.frame() %>% 
   rename(fullname = 1) %>%
@@ -84,32 +83,63 @@ my_h5_files <- 1:nrow(my_xys) %>%
   purrr::map(~find_which_file(.x)) %>% 
   unlist() %>% unique()
 
+### extract time from OBS raster
+### for each file overlapping point
+
 my_h5_file <- my_h5_files[1]
 
-file_h5 <- hdf5r::H5File$new(my_h5_file, mode = 'r')
-site <- file_h5$ls()$name
-obs_path <- glue::glue('{site}/Radiance/Metadata/Ancillary_Rasters/OBS_Data')
-my_info <- h5readAttributes(my_h5_file, obs_path)
+extract_gpstime <- function(my_h5_file, my_aq_prj_sp){
+  file_h5 <- hdf5r::H5File$new(my_h5_file, mode = 'r')
+  site <- file_h5$ls()$name
+  obs_path <- glue::glue('{site}/Radiance/Metadata/Ancillary_Rasters/OBS_Data')
+  my_info <- h5readAttributes(my_h5_file, obs_path)
+  my_nrows <- my_info$Dimensions[2]
+  my_ncols <- my_info$Dimensions[1]
+  # gps time is the 10th band
+  obs1 <- h5read(my_h5_file, obs_path, index = list(10, 1:my_nrows, 1:my_ncols))
+  obs1_matrix <- matrix(obs1[1,,], nrow = my_nrows, ncol = my_ncols)
+  
+  # save this for getting all obs data layers
+  # my_stack <- purrr::map(1:10, ~matrix(obs1[.x,,], nrow = 8264, ncol = 1017)) %>%
+  #   purrr::map(~raster(.x)) %>%
+  #   stack()
+  # ids <- values(my_stack[[1]]) == my_info$Data_Ignore_Value
+  # my_stack[ids] <- NA
+  
+  # make sure to transpose!! 
+  my_r <- raster(t(obs1_matrix))
+  # replace No data pixels
+  ids <- values(my_r[[1]]) == my_info$Data_Ignore_Value
+  my_r[ids] <- NA
+  
+  # set extent and CRS
+  extent(my_r) <- raster::extent(hs_mapinfo(my_h5_file)[["xmin"]],
+                                 hs_mapinfo(my_h5_file)[["xmax"]],
+                                 hs_mapinfo(my_h5_file)[["ymin"]],
+                                 hs_mapinfo(my_h5_file)[["ymax"]])
+  crs(my_r) <- my_epsg
+  
+  plot(my_r)
+  plot(my_aq_prj_sp, add = TRUE, pch = 4, col = 'blue', cex = 1)
+  
+  # extract spectra from pixels within the 5m buffered point
+  my_pt_spatvec <- my_aq_prj_sp %>% as('SpatVector')
+  my_hs_terra <- my_r %>% terra::rast()
+  
+  my_gpstime <- terra::extract(my_hs_terra, my_pt_spatvec, cells = TRUE)
+  gsp_df <- my_gpstime %>%
+    rename(gps_time = layer) %>% 
+    mutate(x = terra::xFromCell(my_hs_terra, cell),
+           y = terra::yFromCell(my_hs_terra, cell)) %>%
+    mutate(filename = basename(my_h5_file)) %>%
+    mutate(date = str_sub(filename, 19, 26)) %>%
+    mutate(datestring = glue('{date} {hms(hours = gps_time)}')) %>%
+    mutate(datetime = lubridate::as_datetime(datestring))
+  
 
-obs1 <- h5read(my_h5_file, obs_path, index = list(10, 1:my_info$Dimensions[2], 1:my_info$Dimensions[1]))
-obs1_matrix <- matrix(obs1[1,,], nrow = my_info$Dimensions[2], ncol = my_info$Dimensions[1])
-# save this for getting all obs data layers
-# my_stack <- purrr::map(1:10, ~matrix(obs1[.x,,], nrow = 8264, ncol = 1017)) %>%
-#   purrr::map(~raster(.x)) %>%
-#   stack()
-# ids <- values(my_stack[[1]]) == my_info$Data_Ignore_Value
-# my_stack[ids] <- NA
+  return(gsp_df)
+}
 
-my_r <- raster(obs1_matrix)
-ids <- values(my_r[[1]]) == my_info$Data_Ignore_Value
-my_r[ids] <- NA
 
-plot(my_r)
-
-# put the obs time raster in right map info
-# in order to be able to extract values for
-# site location coordinates
-
-# for multiple flight lines...
-# get time from relevant pixels for each date
-
+times_df <- my_h5_files %>% purrr::map_dfr(~extract_gpstime(.x, my_aq_prj_sp))
+times_df
