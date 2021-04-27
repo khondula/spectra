d5d0b7306d945146e6715b1fe96c110d7ac8022a
kelly hondula <hondula@battelleecology.org>
2021-04-27 18:57:53 -0400

    rough cut of model no phyto backs or sus matter

--- 06-closest-grab-data.R
+++ 06-closest-grab-data.R
@@ -1,14 +1,16 @@
 # get AOS data based on date and site
 library(glue)
+library(tidyverse)
+library(lubridate)
 
 rad_times_dir <- '/Volumes/hondula/DATA/radiance-time'
 
-my_aq_site <- 'BLWA'
-my_aop_yr <- '2017'
+my_aq_site <- 'BARC'
+my_aop_yr <- '2019'
 my_loc_type <- 'buoy.c0'
 
 times_df <- fs::dir_ls(rad_times_dir, glob = glue('*{my_aq_site}*{my_aop_yr}*')) %>%
-  read_csv()
+  read_csv() %>% filter(!is.na(gps_time))
 
 times_df
 
@@ -16,13 +18,14 @@ times_df
 # has the best weather? 
 
 my_time <- times_df[['datetime']][1]
+my_date <- as_date(my_time)
 
 # get grab sample data for site closest to my_time
-suva254_path <- 'suva254_all.csv'
-my_analyte <- 'SUVA254'
+# suva254_path <- 'suva254_all.csv'
+# my_analyte <- 'SUVA254'
 
-grab_path <- suva254_path
-grab_path <- 'suva280_all.csv'
+# grab_path <- suva254_path
+# grab_path <- 'suva280_all.csv'
 
 grab_paths <- list(SUVA254 = 'suva254_all.csv',
                    SUVA280 = 'suva280_all.csv',
@@ -34,7 +37,13 @@ my_analyte <- 'chla'
 
 get_closest_sample <- function(my_analyte){
   
-  grab_path <-grab_paths[[my_analyte]]
+  grab_path <- grab_paths[[my_analyte]]
+  
+  grab_paths <- list(SUVA254 = 'suva254_all.csv',
+                     SUVA280 = 'suva280_all.csv',
+                     doc = 'doc_all.csv',
+                     tss = 'tss_all.csv',
+                     chla = 'all-phyto-data.csv')
   
   grab_df <- glue('../neon-sites/results/{grab_path}') %>% 
     read_csv() %>%
@@ -45,7 +54,7 @@ get_closest_sample <- function(my_analyte){
   if(my_analyte %in% c('chla')){
     grab_df <- grab_df %>%
       dplyr::select(-collectDate) %>%
-      dplyr::rename(collectDate = collect_date)
+      dplyr::mutate(collectDate = lubridate::as_datetime(glue('{collect_date} {time_hms}')))
   }
 
 # ggplot(grab_df, aes(x = collectDate, y = analyteConcentration)) +
@@ -54,19 +63,23 @@ get_closest_sample <- function(my_analyte){
 #   geom_point() +
 #   theme_bw() +
 #   ylim(0, NA) +
+#   facet_wrap(vars(analyte), ncol = 1) +
 #   ggtitle(glue('{my_aq_site} {my_aop_yr} {my_analyte}'))
 
 # time difference is the number of days before (-) 
 # or after (+) the flight time that the grab sampling occurred
-time_diff_df <- grab_df %>%
-  dplyr::mutate(time_diff_days = difftime(collectDate, my_time, units = 'day')) %>% 
-  mutate(time_diff_days = round(time_diff_days, 1)) %>% 
-  mutate(min_diff = time_diff_days == min(abs(time_diff_days)))
+  time_diff_df <- grab_df %>%
+    dplyr::mutate(time_diff_days = difftime(collectDate, my_time, units = 'day')) %>% 
+    mutate(time_diff_days = round(time_diff_days, 1)) %>%
+    mutate(time_diff_abs = abs(time_diff_days)) %>%
+    mutate(min_diff = time_diff_abs == min(time_diff_abs))
 
-closest_sample <- time_diff_df %>% filter(min_diff)
-return(closest_sample)
+  closest_sample <- time_diff_df %>% filter(min_diff)
+  return(closest_sample)
 }
 
 my_analytes <- c('SUVA254', 'SUVA280', 'doc', 'tss', 'chla')
 my_samps <- my_analytes %>% purrr::map(~get_closest_sample(.x))
 names(my_samps) <- my_analytes
+
+my_samps$chla %>% View()
