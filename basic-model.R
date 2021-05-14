f405c83de648f90764457f3a673102404c58c373
kelly hondula <hondula@battelleecology.org>
2021-05-14 15:56:32 -0400

    basic app to model spectra

--- basic-model.R
+++ basic-model.R
@@ -22,10 +22,16 @@ sbSED <- 1
 sbPART <- (scatterCHL443 * sbCHL + scatterSED443 * sbSED)/(scatterCHL443 + scatterSED443)
 sbPART
 
+# WATER data
+water3 <- read_excel("../../data/water-spectra/water-spectra-buiteveld94.xlsx") %>%
+  dplyr::filter(lambda_nm >= 400)
+water_wls <- water3$lambda_nm
+
+# water3 %>% write_csv("bo-model-app/data/water.csv")
 # MODELS
 
 calc_absorb <- function(wl, absREF, aSF =  1.3e-2, wlREF = 443){
-  abs_wl <- absREF * exp(-SF * (wl - wlREF))
+  abs_wl <- absREF * exp(-aSF * (wl - wlREF))
   return(abs_wl)
 }
 
@@ -33,8 +39,6 @@ calc_scatter <- function(wl, scatterREF, bSF = 1, wlREF = 443){
   scatter_wl <- scatterREF * (wl/wlREF)^(bSF)
   return(scatter_wl)
 }
-
-# calc_scatter(400, scatterCHL443, 1)
 
 calc_scatter_spectra <- function(scatterREF, bSF =  1,
                                  bratio = 0.01, wlREF = 443, 
@@ -50,7 +54,7 @@ calc_scatter_spectra <- function(scatterREF, bSF =  1,
   return(my_spectra)
 }
 
-calc_absorb_spectra <- function(absREF,  =  1.3e-2, wlREF = 443, 
+calc_absorb_spectra <- function(absREF,  aSF =  1.3e-2, wlREF = 443, 
                          spectra_min = 400, spectra_max = 800){
   wl_range <- spectra_min:spectra_max
   my_spectra <- wl_range %>% 
@@ -59,19 +63,79 @@ calc_absorb_spectra <- function(absREF,  =  1.3e-2, wlREF = 443,
     rename(abs_m1 = value) %>%
     mutate(wavelength = wl_range) %>%
     mutate(wavelength = as.numeric(wavelength)) %>%
-    mutate(wlREF = wlREF, SF = SF)
+    mutate(wlREF = wlREF, SF = aSF)
   return(my_spectra)
 }
 
 
+# ABSORBANCE
+abs_water = data.frame(wl = water3$lambda_nm,
+                       abs_water = water3$absorption_m1)
 
-ABSnap_spectra <- calc_absorb_spectra(absNAP443, aSF = sNAP)
-ABScdom_spectra <- calc_absorb_spectra(absCDOM443, bSF = sCDOM)
+ABSnap_spectra <- calc_absorb_spectra(absNAP443, aSF = sNAP) %>%
+  rename(wl = wavelength, abs_nap = abs_m1) %>%
+  dplyr::select(wl, abs_nap)
+ABScdom_spectra <- calc_absorb_spectra(absCDOM443, aSF = sCDOM) %>%
+  rename(wl = wavelength, abs_cdom = abs_m1) %>%
+  dplyr::select(wl, abs_cdom)
 
-BSchl_spectra <- calc_scatter_spectra(scatterCHL443, sbCHL, bratioCHL)
-BSsed_spectra <- calc_scatter_spectra(scatterSED443, sbSED, bratioSED)
+abs_df <- abs_water %>%
+  left_join(ABSnap_spectra) %>%
+  left_join(ABScdom_spectra) %>%
+  mutate(abs_total = abs_water + abs_nap + abs_cdom)
 
+# BACKSCATTERING
+backs_water = data.frame(wl = water3$lambda_nm,
+                         backs_water = 0.5*(water3$scattering_m1))
+BSchl_spectra <- calc_scatter_spectra(scatterCHL443, sbCHL, bratioCHL) %>%
+  rename(wl = wavelength, bb_chl = backscatter_m1) %>%
+  dplyr::select(wl, bb_chl)
+BSsed_spectra <- calc_scatter_spectra(scatterSED443, sbSED, bratioSED) %>%
+  rename(wl = wavelength, bb_sed = backscatter_m1) %>%
+  dplyr::select(wl, bb_sed)
 
+backs_df <- backs_water %>%
+  left_join(BSchl_spectra) %>%
+  left_join(BSsed_spectra) %>%
+  mutate(backs_total = backs_water + bb_chl + bb_sed)
 
+mu0 = 1
 C_mu0 = -0.629 * mu0 + 0.975
 surface_factor = 0.544
+
+rs_df <- abs_df %>% left_join(backs_df) %>%
+  mutate(rD = 0.544 * C_mu0 * (backs_total/ (abs_total + backs_total)))
+
+rs1 <- rs_df %>% 
+  dplyr::filter(wl %in% water_wls) %>%
+  ggplot(aes(x = wl, y = rD)) +
+  geom_line() +
+  theme_bw() +
+  ggtitle("Reflectance")
+rs2 <- rs_df %>% 
+  dplyr::filter(wl %in% water_wls) %>%
+  ggplot(aes(x = wl, y = abs_total)) +
+  geom_line(lwd = 1) +
+  geom_line(aes(y = abs_nap), col = "brown") +
+  geom_line(aes(y = abs_cdom), col = "orange") +
+  geom_line(aes(y = abs_water), col = "blue") +
+  theme_bw() +
+  ggtitle("Absorbance")
+rs3 <- rs_df %>% 
+  dplyr::filter(wl %in% water_wls) %>%
+  ggplot(aes(x = wl, y = backs_total)) +
+  geom_line(lwd = 1) +
+  geom_line(aes(y = bb_chl), col = "green") +
+  geom_line(aes(y = bb_sed), col = "orange") +
+  geom_line(aes(y = backs_water), col = "blue") +
+  theme_bw() +
+  ggtitle("Backscattering")
+
+gg <- cowplot::plot_grid(rs1, rs2, rs3, 
+                   ncol = 1, rel_heights = c(1, 0.8, 0.8))
+
+gg1 <- rs1 +
+  geom_point_interactive(aes(tooltip = wl, data_id = wl), size = 1) 
+x <- girafe(code = print(gg1))
+x
+
