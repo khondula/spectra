af57938e75be05def0dc8b4af3960f40aa298af8
Kelly Hondula (US) <hondula@battelleecology.org>
2021-05-27 16:53:16 -0600

    the answer is covnerting to terra!!!

--- proj-troubleshooting.R
+++ proj-troubleshooting.R
@@ -2,11 +2,11 @@
 
 filename <- "D:/2017/FullSite/D09/2017_WOOD_2/L3/Spectrometer/Reflectance/NEON_D09_WOOD_DP3_490000_5222000_reflectance.h5"
 bands <- 1:100
-use_h5_extent <- TRUE
 
-h5_extent <- hs_extent(my_h5_files[1])
+use_h5_extent <- TRUE
+h5_extent <- hs_extent(filename)
 out_extent <- h5_extent
-dims <- hs_dims(my_h5_files[1])
+dims <- hs_dims(filename)
 index <- list(bands, seq_len(dims[2]), seq_len(dims[3]))
 
 hs_clean <- function(vals, reflectance) {
@@ -33,16 +33,10 @@ read_hs_values <- function(filename, index){
     
     r <- raster::t(raster::stack(arrays))
     r <- hs_clean(r, reflectance)
-  #  file_h5$close_all()
-    
-      mapinfo_path <- glue::glue('{site}/Reflectance/Metadata/Coordinate_System/Proj4')
-      my_proj4 <- file_h5[[mapinfo_path]]$read()
-
-    proj4string(r) <- my_proj4
-    crs(r) <- "+proj=UTM +zone=14 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
-    raster::projection(r) <- my_proj4
     names(r) <- hs_wavelength(filename, bands)
-    r
+  
+    r2 <- r %>% terra::rast()
+    terra::crs(r2) <- st_crs(my_epsg) %>% as.character()
   }
   
 r <- read_hs_values(my_h5_files[1], index)
